6885c54fb4b9b00d342e520dc108c2deccc2b713
Joemoe117 <babalegrunt@hotmail.fr>
2014-04-13 18:42:02 +0200

    #1
    Premier partie du Commit
    Création du repository avec codeigniter et configuration des fichiers et
    dossier
    Ajout des libraries JQuery et Bootstrap

---
+++ application/views/template/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+
+    <title>Point-it</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href=" <?php echo base_url("/assets/css/bootstrap.min.css"); ?>" rel="stylesheet">
+
+
+    <script src=" <?php echo base_url("/assets/js/jquery-2.1.0.min.js"); ?> "></script>
+    <script src=" <?php echo base_url("/assets/js/bootstrap.min.js"); ?> "></script>
+
+  </head>
+
+
+
+  <body>
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Point-it</a>
+        </div>
+        <div class="navbar-collapse collapse ">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Accueil</a></li>
+            <li><a href="#about">Profil</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container theme-showcase" role="main">
+
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>Hello, world!</h1>
+        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <p><a href="#" class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a></p>
+      </div>
+
+    </div> <!-- /container -->
+  </body>
+</html>
