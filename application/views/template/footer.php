dd8e7f3f6f869f2cd84b3a22c372cb2499848f30
Baptiste BALLAN <babalegrunt@hotmail.fr>
2014-04-14 00:46:47 +0200

    Correction bug footer
    Correction d'un bug dans le footer avec une double balise footer
    correction des couleurs du footer

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,23 +1,21 @@
 		<div class="footer">
 			<div class="container">	
-				<div class="footer">
-					<div class="row">
-						<div class="col-md-4">
-							<div class="footer_titre"> Point-it </div>
-							Point-it est le fils spirituel, un projet plutôt stylé qui a démarré en 2012.
-						</div>
-						<div class="col-md-4">
-							<div class="footer_titre"> Crédits </div>
-							Ce site a été codé par <b>Baptiste</b> et <b>Thomas</b>, deux développeurs alcooliques de qualité! <br>
-							Le design sera fait par quelqu'un qui sera suffisament gentil pour vouloir nous aider
-						</div>
-						<div class="col-md-4">
-							<div class="footer_titre"> Technologies utilisées </div>
-							<img src="http://hypothesisinfolab.com/_include/img/profile/codeigniter_logo.png" alt="CodeIgniter, framework de qualité" width="80">
-							<img src="http://hamedinfo.com/images/jquery_logo.gif" alt="Jquery, parce que sinon le JS c'est chiant" width="80">
-							<img src="http://marcmalignan.com/img/logos/bootstrap.png" alt="Boostrap, cause i'm not a webdesigner" width="80">
-							<img src="http://www.matthewfarmer.net/assets/github_logo-3a39ffd6fcf6f63f799b59e5aad34680.png" alt="Github, Parce que plus on de fous, plus on rit" width="80">
-						</div>
+				<div class="row">
+					<div class="col-md-4">
+						<div class="footer_titre"> Point-it </div>
+						Point-it est le fils spirituel, un projet plutôt stylé qui a démarré en 2012.
+					</div>
+					<div class="col-md-4">
+						<div class="footer_titre"> Crédits </div>
+						Ce site a été codé par <b>Baptiste</b> et <b>Thomas</b>, deux développeurs alcooliques de qualité! <br>
+						Le design sera fait par quelqu'un qui sera suffisament gentil pour vouloir nous aider
+					</div>
+					<div class="col-md-4">
+						<div class="footer_titre"> Technologies utilisées </div>
+						<img src="http://hypothesisinfolab.com/_include/img/profile/codeigniter_logo.png" alt="CodeIgniter, framework de qualité" width="80">
+						<img src="http://hamedinfo.com/images/jquery_logo.gif" alt="Jquery, parce que sinon le JS c'est chiant" width="80">
+						<img src="http://marcmalignan.com/img/logos/bootstrap.png" alt="Boostrap, cause i'm not a webdesigner" width="80">
+						<img src="http://www.matthewfarmer.net/assets/github_logo-3a39ffd6fcf6f63f799b59e5aad34680.png" alt="Github, Parce que plus on de fous, plus on rit" width="80">
 					</div>
 				</div>
 			</div>
