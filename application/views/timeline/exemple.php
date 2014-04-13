f03297f7657af930650e2ff924be8d355d85cf07
Baptiste BALLAN <babalegrunt@hotmail.fr>
2014-04-14 00:26:28 +0200

    Ajout d'exemple de post sur la timeline
    Ajout d'exemple de post sur la timeline
    MàJ du css

--- application/views/timeline/exemple.php
+++ application/views/timeline/exemple.php
@@ -1,6 +1,47 @@
 <div class="container main">
 
 	<div class="col-md-8">
+		<h1>Timeline</h1>
+
+
+		<h2>Test 1</h2>
+		<div class="point">
+		<img src="<?php echo base_url("/assets/images/profil.jpg");  ?>" alt="..." class="img-rounded" width="60">
+		<span class="point_texte"> Baptiste a gagné un point Moustache </span> <br>
+			
+			<div class="commentaire">
+				<img src="<?php echo base_url("/assets/images/profil.jpg");  ?>" alt="..." class="img-rounded" width="30">
+				<span class="commentaire_texte"> Ah ouais, t'es vraiment un mec stylé ! </span>
+			</div>
+		</div>	
+
+
+
+
+
+
+
+
+
+
+
+		<h2>Test 2</h2>
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<img src="<?php echo base_url("/assets/images/profil.jpg");  ?>" alt="..." class="img-rounded" width="40">
+				<span class="point_texte"> Baptiste a gagné un point Moustache <span class="glyphicon glyphicon-thumbs-up pull-right">7</span>	</span> <br>
+
+
+			</div>
+			<div class="panel-body">
+				<div class="commentaire_2">
+				<img src="<?php echo base_url("/assets/images/profil.jpg");  ?>" alt="..." class="img-rounded" width="32">
+				<span class="commentaire_texte"> Ah ouais, t'es vraiment un mec stylé ! </span>
+			</div>
+			</div>
+		</div> 
+
+		<br><br><br><br>
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
