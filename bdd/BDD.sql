936105fd1c4f0b3a596f678810d7c53059c9d4a2
Thoumou <gauvin.th@gmail.com>
2014-07-25 17:40:05 +0200

    Fixe du leaderboard

--- bdd/BDD.sql
+++ bdd/BDD.sql
@@ -1,51 +1,52 @@
-CREATE TABLE `Profils` (
+CREATE TABLE `profils` (
 	`profil_id` INT AUTO_INCREMENT PRIMARY KEY,
 	`profil_nom` VARCHAR(30) NOT NULL UNIQUE,
 	`profil_pass` VARCHAR(20) NOT NULL,
 	`profil_image` VARCHAR(255)
 ) ENGINE = InnoDB;
 
-CREATE TABLE `Types_Point` (
-	`typept_nom` VARCHAR(30) PRIMARY KEY,
+CREATE TABLE `types_point` (
+	`typept_id` INT PRIMARY KEY,
+	`typept_nom` VARCHAR(30) NOT NULL,
 	`typept_description` TEXT NOT NULL,
 	`typept_image` VARCHAR(255),
 	`typept_success` VARCHAR(255)
 ) ENGINE = InnoDB;
 
-CREATE TABLE `Points`(
+CREATE TABLE `points`(
 	`point_id` INT AUTO_INCREMENT PRIMARY KEY,
-	`typept_nom` VARCHAR(30) NOT NULL,
+	`typept_id` INT NOT NULL,
 	`profil_id_donne` INT NOT NULL,
 	`point_description` TEXT NOT NULL,
 	`point_date_crea` TIMESTAMP NOT NULL,
 	`point_date_evenement` TIMESTAMP,
-	FOREIGN KEY (`typept_nom`) REFERENCES `Types_Point`(`typept_nom`),
-	FOREIGN KEY (`profil_id_donne`) REFERENCES `Profils`(`profil_id`)
+	FOREIGN KEY (`typept_id`) REFERENCES `types_point`(`typept_id`),
+	FOREIGN KEY (`profil_id_donne`) REFERENCES `profils`(`profil_id`)
 ) ENGINE = InnoDB;
 
-CREATE TABLE `Recoit`(
+CREATE TABLE `recoit`(
 	`point_id` INT,
 	`profil_id` INT,
 	PRIMARY KEY (`point_id`, `profil_id`),
-	FOREIGN KEY (`point_id`) REFERENCES `Points`(`point_id`),
-	FOREIGN KEY (`profil_id`) REFERENCES `Profils`(`profil_id`)
+	FOREIGN KEY (`point_id`) REFERENCES `points`(`point_id`),
+	FOREIGN KEY (`profil_id`) REFERENCES `profils`(`profil_id`)
 ) ENGINE = InnoDB;
 
-CREATE TABLE `Commentaires` (
+CREATE TABLE `commentaires` (
 	`com_id` INT AUTO_INCREMENT PRIMARY KEY,
 	`point_id` INT NOT NULL,
 	`profil_id` INT NOT NULL,
 	`com_texte` TEXT NOT NULL,
 	`com_date` TIMESTAMP NOT NULL,
-	FOREIGN KEY (`point_id`) REFERENCES `Points`(`point_id`),
-	FOREIGN KEY (`profil_id`) REFERENCES `Profils`(`profil_id`)
+	FOREIGN KEY (`point_id`) REFERENCES `points`(`point_id`),
+	FOREIGN KEY (`profil_id`) REFERENCES `profils`(`profil_id`)
 ) ENGINE = InnoDB;
 
-CREATE VIEW `Liste_Points` AS
-	SELECT `profil_id`, `point_id`, `typept_nom`
-	FROM `Recoit`
-	NATURAL JOIN `Profils`
-	NATURAL JOIN `Points`
-	NATURAL JOIN `Types_Point`
+CREATE VIEW `liste_points` AS
+	SELECT `profil_id`, `point_id`, `typept_id`
+	FROM `recoit`
+	NATURAL JOIN `profils`
+	NATURAL JOIN `points`
+	NATURAL JOIN `types_point`
 	ORDER BY `profil_id`;
 	
